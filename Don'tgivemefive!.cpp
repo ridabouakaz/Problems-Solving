bc3f69dc52f6812f53b68eb6db8d5143c354dc97
ridabouakaz <bouakaz99@gmail.com>
2024-08-16 06:11:25 +0100

    solution dont give me five

---
+++ Don'tgivemefive!.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include <array>
+#include<cmath>
+#include <string>
+#include <iostream>
+#include <string>
+using namespace std;
+int dontGiveMeFive(int start, int end) {
+	int result = 0;
+	if (end > 9)
+	{
+		result = (end - start) - 1;
+	}
+	else
+	{
+		result = (end - start);
+	}
+	return result;
+}
+
+int main()
+{
+	cout << dontGiveMeFive(4, 17);
+	return 0;
+}
+/*Description:
+Don't give me five!
+
+In this kata you get the start number and the end number of a region and should return the count of all numbers except numbers with a 5 in it. The start and the end number are both inclusive!
+
+Examples:
+
+1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+
+The result may contain fives. ;-)
+The start number will always be smaller than the end number. Both numbers can be also negative!
+
+I'm very curious for your solutions and the way you solve it. Maybe someone of you will find an easy pure mathematics solution.
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have also created other katas. Take a look if you enjoyed this kata!
+*/
