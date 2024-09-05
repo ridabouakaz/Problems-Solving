a6bc2907a4cac8fb0ec3563056dd89cb5642fc6a
ridabouakaz <bouakaz99@gmail.com>
2024-09-05 07:00:50 +0100

    Solution Count The Smiley Faces1

---
+++ Countthesmileyfaces!.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include <string>
+#include <sstream>
+#include <climits>
+using namespace std;
+int countSmileys(vector<string> arr) {
+	int count = 0;
+	int result = 0;
+	for (string word : arr)
+	{
+		for (int i = 0; i < word.size(); i++)
+		{
+			if (word[i] == ';' || word[i] == ':')
+			{
+				count++;
+			}
+			else if (word[i] == ')' || word[i] == 'D')
+			{
+				count--;
+			}
+			else if (word[i] == '-' || word[i] == '~')
+			{
+				continue;
+			}
+			else
+			{
+				break;
+			}
+
+		}
+		if (count == 0)
+		{
+			result++;
+			count = 0;
+		}
+		count = 0;
+	}
+	return result;
+}
+int main()
+{
+	cout << countSmileys({ ";-(", ";>", ":)", ":~>", ";>", ":>", ";o)" });
+	return 0;
+}
+/*Description:
+
+Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+
+Rules for a smiling face:
+
+	Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+	A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+	Every smiling face must have a smiling mouth that should be marked with either ) or D
+
+No additional characters are allowed except for those mentioned.
+
+Valid smiley face examples: :) :D ;-D :~)
+Invalid smiley faces: ;( :> :} :]
+Example
+
+countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
+countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
+countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
+
+Note
+
+In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
+Regular Expressions
+Fundamentals*/
