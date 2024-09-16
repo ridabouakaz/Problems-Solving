4905d84e74f7c6847f3355b69c156d8e0626b0f6
ridabouakaz <bouakaz99@gmail.com>
2024-09-16 05:12:32 +0100

    Solution Is a Number Prime ?

---
+++ Isanumberprime.cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include <string>
+#include <sstream>
+#include <climits>
+#include<iomanip>
+using namespace std;
+bool is_prime(int num) {
+    if (num <= 1) return false;
+    if (num == 2) return true;
+    if (num % 2 == 0) return false;
+    for (int i = 3; i * i <= num; i += 2) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    cout << is_prime(23);
+    return 0;
+}
+/*Description:
+
+Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
+
+Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+Requirements
+
+    You can assume you will be given an integer input.
+    You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
+    NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 ( or similar, depending on language ). Looping all the way up to n, or n/2, will be too slow.
+
+Example
+
+is_prime(1)  false
+is_prime(2)  true
+is_prime(-1) false
+Mathematics
+Algorithms*/
+//          ／＞　　フ
+//　　　　　| 　n　n 彡
+//　 　　　／`ミ＿xノ
+//　　 　 /　　　 　 |
+//　　　 /　 ヽ　　 ﾉ
+//　 　 │　　|　|　|
+//　／￣|　　 |　|　|
+//　| (￣ヽ＿_ヽ_)__)
+//　＼二つ
+// ITS CAT FOR YOU
