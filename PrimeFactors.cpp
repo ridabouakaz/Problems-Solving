493383ddce2164bcf6f6dd7e88ec48a7191f64f0
ridabouakaz <bouakaz99@gmail.com>
2024-09-18 06:53:24 +0100

    Solution Prime Factors

---
+++ PrimeFactors.cpp
@@ -0,0 +1,61 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include <string>
+#include <sstream>
+#include <climits>
+#include<iomanip>
+using namespace std;
+vector<int> prime_factors(long n) {
+    vector<int> factors;
+    if (n < 2) {
+        return factors;
+    }
+    while (n % 2 == 0) {
+        factors.push_back(2);
+        n /= 2;
+    }
+    for (long i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            factors.push_back(i);
+            n /= i;
+        }
+    }
+    if (n > 2) {
+        factors.push_back(n);
+    }
+    return factors;
+}
+int main()
+{
+    prime_factors(3);
+    return 0;
+}
+/*Description:
+Prime Factors
+
+Inspired by one of Uncle Bob's TDD Kata
+
+Write a function that generates factors for a given number.
+
+The function takes an integer as parameter and returns a list of integers (ObjC: array of NSNumbers representing integers). That list contains the prime factors in numerical sequence.
+Examples
+
+1  ==>  []
+3  ==>  [3]
+8  ==>  [2, 2, 2]
+9  ==>  [3, 3]
+12 ==>  [2, 2, 3]
+
+Algorithms*/
+//          ／＞　　フ
+//　　　　　| 　n　n 彡
+//　 　　　／`ミ＿xノ
+//　　 　 /　　　 　 |
+//　　　 /　 ヽ　　 ﾉ
+//　 　 │　　|　|　|
+//　／￣|　　 |　|　|
+//　| (￣ヽ＿_ヽ_)__)
+//　＼二つ
+// ITS CAT FOR YOU
