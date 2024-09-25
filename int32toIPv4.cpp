d22fabeaefaf628c19576d1762c998131e0e73a7
ridabouakaz <bouakaz99@gmail.com>
2024-09-25 03:52:35 +0100

    Solution int32 to IPv4

---
+++ int32toIPv4.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+string uint32_to_ip(uint32_t ip) {
+    return to_string((ip >> 24) & 0xFF) + '.' +
+        to_string((ip >> 16) & 0xFF) + '.' +
+        to_string((ip >> 8) & 0xFF) + '.' +
+        to_string(ip & 0xFF);
+}
+int main()
+{
+    cout << uint32_to_ip(2154959208);
+    return 0;
+}
+/*Description:
+
+Take the following IPv4 address: 128.32.10.1
+
+This address has 4 octets where each octet is a single byte (or 8 bits).
+
+    1st octet 128 has the binary representation: 10000000
+    2nd octet 32 has the binary representation: 00100000
+    3rd octet 10 has the binary representation: 00001010
+    4th octet 1 has the binary representation: 00000001
+
+So 128.32.10.1 == 10000000.00100000.00001010.00000001
+
+Because the above IP address has 32 bits, we can represent it as the unsigned 32 bit number: 2149583361
+
+Complete the function that takes an unsigned 32 bit number and returns a string representation of its IPv4 address.
+Examples
+
+2149583361 ==> "128.32.10.1"
+32         ==> "0.0.0.32"
+0          ==> "0.0.0.0"
+
+Networks
+Bits
+Algorithms*/
+//          ／＞　　フ
+//　　　　　| 　n　n 彡
+//　 　　　／`ミ＿xノ
+//　　 　 /　　　 　 |
+//　　　 /　 ヽ　　 ﾉ
+//　 　 │　　|　|　|
+//　／￣|　　 |　|　|
+//　| (￣ヽ＿_ヽ_)__)
+//　＼二つ
+// ITS CAT FOR YOU
